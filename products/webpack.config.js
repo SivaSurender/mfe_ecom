0be4e7be84bdb34fc5b441e157a2825b39f7f68a
SivaSurender <e.surender14@gmail.com>
2024-10-19 23:06:35 +0530

    html webpack plugin

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,3 +1,12 @@
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
+  devServer: { port: 8081 },
+  plugins: [
+    //  take all dynamic js fiels coming out from webpack and insert in index.html
+    new htmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
