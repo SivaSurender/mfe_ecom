f25f5d33f006c9ae43c1cac2fe269d58d25a5594
SivaSurender <e.surender14@gmail.com>
2024-10-22 19:35:02 +0530

    setup

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,5 +1,5 @@
 const htmlWebpackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 module.exports = {
   mode: "development",
   devServer: { port: 8080 },
@@ -9,10 +9,11 @@ module.exports = {
       template: "./public/index.html",
     }),
     new ModuleFederationPlugin({
-      name:"container",
-      remotes:{
-        products:"products@http://localhost:8081/remoteEntry.js"
-      }
-    })
+      name: "container",
+      remotes: {
+        products: "products@http://localhost:8081/remoteEntry.js",
+        cart: "cart@http://localhost:8082/remoteEntry.js",
+      },
+    }),
   ],
 };
